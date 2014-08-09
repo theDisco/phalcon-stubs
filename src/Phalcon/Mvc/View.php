ae0f0c81324e5d5fb8b5ada8cc21a46d250c316b
Wojtek Gancarczyk <gancarczyk@gmail.com>
2014-08-09 19:25:10 +0200

    added stubs for 1.2.4

--- src/Phalcon/Mvc/View.php
+++ src/Phalcon/Mvc/View.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 namespace Phalcon\Mvc {
 
@@ -23,7 +23,7 @@ namespace Phalcon\Mvc {
 	 * echo $view->getContent();
 	 * </code>
 	 */
-
+	
 	class View extends \Phalcon\DI\Injectable implements \Phalcon\Events\EventsAwareInterface, \Phalcon\DI\InjectionAwareInterface, \Phalcon\Mvc\ViewInterface {
 
 		const LEVEL_MAIN_LAYOUT = 5;
